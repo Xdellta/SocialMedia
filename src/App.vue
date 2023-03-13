361fdc15fd0b74691615cec53008d47a2bebc0a3
Xdellta <66367896+Xdellta@users.noreply.github.com>
2023-03-13 22:42:43 +0100

    AsideMenu, data
    - Field for filtering groups and friends
    - Section with user groups
    - User friends section
    - .json files containing user, all user and group data

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,33 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import AsideMenu from './components/AsideMenu.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <aside>
+      <AsideMenu />
+    </aside>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
+<style type="scss" scoped>
+  main {
+    width: 100%;
+    height: 100%;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+  aside {
+    position: fixed;
+    height: 100%;
+    width: 350px;
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    align-items: center;
+    flex-direction: column;
+    background-color: var(--color-white);
+    padding: 35px 30px 0 30px;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/AsideMenu.vue
@@ -0,0 +1,263 @@
+<script>
+  import IconSearch from './icons/IconSearch.vue'
+
+  import dataGroups from '../assets/data/data-groups.json'
+  import dataMe from '../assets/data/data-me.json'
+  import dataUsers from '../assets/data/data-users.json'
+
+  export default {
+    components: {
+      IconSearch
+    },
+
+    name: 'MyComponent',
+    data() {
+      return {
+        myGroups: [],
+        myFriends: [],
+        showMoreGroups: false,
+        showMoreFriends: false,
+        originalGroups: [],
+        originalFriends: []
+      }
+    },
+
+    mounted() {
+      const myGroupIds = dataMe.Me.Groups.map(group => group.ID)
+      this.myGroups = dataGroups.Groups.filter(group => myGroupIds.includes(parseInt(group.ID)))
+      this.originalGroups = this.myGroups.slice()
+
+      const myFriendIds = dataMe.Me.Friends.filter(friend => friend.Status === 'accepted').map(friend => friend.ID)
+      this.myFriends = dataUsers.Users.filter(user => myFriendIds.includes(user.ID))
+      this.originalFriends = this.myFriends.slice()
+
+      this.myFriends.forEach(friend => {
+        if (friend.Status === 'inactive') {
+          const lastActive = new Date(friend.LastActive)
+          const now = new Date()
+          const minutesDiff = Math.round((now - lastActive) / (1000 * 60))
+
+          if (minutesDiff < 60) friend.lastActiveMinutes = minutesDiff + ' min.'
+          if (minutesDiff >= 60) friend.lastActiveMinutes = Math.round(minutesDiff / 60) + ' godz.'
+          if (minutesDiff / 60 >= 24) friend.lastActiveMinutes = Math.round(minutesDiff / 60 / 24) + ' dni'
+          if (Math.round(minutesDiff / 60 / 24) == 1) friend.lastActiveMinutes = '1 dzień'
+          if (minutesDiff / 60 / 24 >= 365) friend.lastActiveMinutes = Math.round(minutesDiff / 60 / 24 / 365) + ' lata'
+          if (Math.round(minutesDiff / 60 / 24 / 365) == 1) friend.lastActiveMinutes = '1 rok'
+        }
+      })
+    },
+
+    methods: {
+      toggleShowMoreGroups() {
+        this.showMoreGroups = !this.showMoreGroups
+      },
+
+      toggleShowMoreFriends() {
+        this.showMoreFriends = !this.showMoreFriends
+      },
+
+      toggleShowMoreFriends() {
+        this.showMoreFriends = !this.showMoreFriends
+      },
+
+      filterData(event) {
+        const keyword = event.target.value.toLowerCase().trim()
+
+        this.myGroups = this.originalGroups.filter(group => {
+          return group.Name.toLowerCase().includes(keyword)
+        })
+
+        this.myFriends = this.originalFriends.filter(friend => {
+          return friend.FirstName.toLowerCase().includes(keyword) || friend.LastName.toLowerCase().includes(keyword)
+        })
+      },
+    },
+  }
+</script>
+
+<template>
+  <div class="form">
+    <button id="Search-btn"><IconSearch class="IconSearch" /></button>
+    <input type="text" placeholder="Wyszukaj" class="SearchInput" v-on:input="filterData">
+  </div>
+
+  <div class="scrollContainer">
+    <div id="groups">
+      <h1 class="title">TWOJE GRUPY:</h1>
+
+      <div class="groups__item" v-for="group in myGroups.slice(0, showMoreGroups ? myGroups.length : 6)" :key="group.ID">
+        <img :src="group.Image" :alt="group.Name" class="item-img">
+        {{ group.Name }}
+      </div>
+
+      <span class="collapse-expand" v-show="myGroups.length > 6" @click="toggleShowMoreGroups">
+        {{ showMoreGroups ? 'POKAŻ MNIEJ' : 'POKAŻ WIĘCEJ' }}
+      </span>
+    </div>
+
+    <div id="friends">
+      <H1 class="title">ZNAJOMI:</H1>
+
+      <div class="friends__item" v-for="friend in myFriends.slice(0, showMoreFriends ? myFriends.length : 6)" :key="friend.ID">
+        <div class="item__personalData">
+          <img :src="friend.Avatar" :alt="friend.FirstName + ' ' + friend.FirstName" class="item-img">
+          {{ friend.FirstName }} {{ friend.LastName }}
+        </div>
+
+        <div class="item__status" :class="{ 'status-active': friend.Status === 'active', 'status-inactive': friend.Status === 'inactive' }">
+          {{ friend.lastActiveMinutes }}
+        </div>
+      </div>
+
+      <span class="collapse-expand" v-show="myFriends.length > 6" @click="toggleShowMoreFriends">
+        {{ showMoreFriends ? 'POKAŻ MNIEJ' : 'POKAŻ WIĘCEJ' }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style type="scss" scoped>
+  /* form */
+  .form {
+    height: 47px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 15px;
+  }
+
+  .ColorTheme-btn {
+    fill: var(--color-darkBlue);
+    width: 40px;
+    height: 40px;
+  }
+
+  .ColorTheme-btn:hover {
+    cursor: pointer;
+    transform: scale(1.02);
+    fill: var(--color-lightBlue);
+  }
+
+  #Search-btn {
+    height: 100%;
+    width: 47px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 12px 0 0 12px;
+    border: none;
+    outline: none;
+    background-color: var(--color-darkBlue);
+  }
+
+  .IconSearch {
+    fill: var(--color-white);
+  }
+
+  .SearchInput {
+    width: 81.5%;
+    height: 47px;
+    padding: 2% 5.5%;
+    border-radius: 0 12px 12px 0;
+    border: none;
+    outline: none;
+    background-color: var(--color-background);
+    color: var(--color-black);
+    font-size: 16px;
+  }
+
+  .SearchInput::placeholder {
+    color: var(--color-darkGray);
+  }
+
+  #groups, #friends {
+    width: 92%;
+    height: max-content;
+    display: flex;
+    flex-direction: column;
+  }
+
+  /* groups & friends */
+  .scrollContainer {
+    width: 100%;
+    height: 100%;
+    overflow-y: auto;
+    padding-left: 12px;
+    margin-bottom: 15px;
+  }
+
+  .scrollContainer::-webkit-scrollbar {
+    display: none;
+  }
+
+  #groups {
+    margin: 8% 0 13% 0;
+  }
+
+  .title {
+    font-size: 17px;
+    color: var(--color-lightBlue);
+    margin-bottom: 10px;
+  }
+
+  .groups__item {
+    width: 100%;
+    height: 33px;
+    display: flex;
+    align-items: center;
+    margin: 6px 0;
+  }
+
+  .friends__item {
+    width: 100%;
+    height: 33px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 9px 0;
+  }
+
+  .groups__item:hover, .friends__item:hover {
+    cursor: pointer;
+    transform: scale(1.01);
+  }
+
+  .item__personalData {
+    display: flex;
+    align-items: center;
+  }
+
+  .item-img {
+    height: 33px;
+    width: 33px;
+    border-radius: 50%;
+    background-color: var(--color-background);
+    margin-right: 15px;
+    object-fit: cover;
+  }
+
+  .status-active {
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background-color: var(--color-lightBlue);
+  }
+
+  .status-inactive {
+    color: var(--color-darkGray);
+    font-size: 15px;
+  }
+
+  .collapse-expand {
+    width: max-content;
+    color: var(--color-darkGray);
+    font-size: 15px;
+    margin: 15px auto 0 auto;
+  }
+
+  .collapse-expand:hover {
+    cursor: pointer;
+    color: var(--color-darkBlue);
+  }
+</style>
