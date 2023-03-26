50ff00d59a4ad6472df01efee7501bd0c659241b
Xdellta <66367896+Xdellta@users.noreply.github.com>
2023-03-26 15:11:39 +0200

    Emoji in posts

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -2,6 +2,7 @@
   import dataPosts from '../assets/data/data-posts.json'
   import dataUsers from '../assets/data/data-users.json'
 
+  import IconAddEmoji from './icons/IconAddEmoji.vue'
   import EmojiLike from './icons/EmojiLike.vue'
   import EmojiLove from './icons/EmojiLove.vue'
   import EmojiHaha from './icons/EmojiHaha.vue'
@@ -12,6 +13,7 @@
 
   export default {
     components: {
+      IconAddEmoji,
       EmojiLike,
       EmojiLove,
       EmojiHaha,
@@ -24,7 +26,8 @@
     data() {
       return {
         postsArray: dataPosts.Posts,
-        usersArray: dataUsers.Users
+        usersArray: dataUsers.Users,
+        reactionsClicked: {}
       }
     },
 
@@ -33,6 +36,22 @@
         return this.usersArray.filter(user => {
           return this.postsArray.map(post => post.UserID).includes(user.ID.toString())
         })
+      }
+    },
+
+    methods: {
+      toggleReaction(postId, reactionName) {
+        if (!this.reactionsClicked[postId]) {
+          this.reactionsClicked[postId] = {}
+        }
+
+        if (this.reactionsClicked[postId][reactionName]) {
+          this.reactionsClicked[postId][reactionName] = false
+          this.postsArray.find(post => post.ID === postId).Reactions.find(r => r.ReactionName === reactionName).ReactionValue--
+        } else {
+          this.reactionsClicked[postId][reactionName] = true
+          this.postsArray.find(post => post.ID === postId).Reactions.find(r => r.ReactionName === reactionName).ReactionValue++
+        }
       }
     }
   }
@@ -60,14 +79,29 @@
         <div class="reactions">
 
           <div class="reactions__item" v-for="reaction in post.Reactions.filter(r => r.ReactionValue > 0)" :key="reaction.ReactionName">
-            <span class="reactionValue" v-if="reaction.ReactionValue > 0">{{ reaction.ReactionValue }}</span>
-            <EmojiLike class="emoji" v-if="reaction.ReactionName === 'Like'" />
-            <EmojiLove class="emoji" v-if="reaction.ReactionName === 'Love'" />
-            <EmojiHaha class="emoji" v-if="reaction.ReactionName === 'Haha'" />
-            <EmojiAngry class="emoji" v-if="reaction.ReactionName === 'Angry'" />
-            <EmojiSad class="emoji" v-if="reaction.ReactionName === 'Sad'" />
-            <EmojiWow class="emoji" v-if="reaction.ReactionName === 'Wow'" />
-            <EmojiYa class="emoji" v-if="reaction.ReactionName === 'Ya'" />
+            <span :class="{ reactionValue: true, active: reactionsClicked[post.ID] && reactionsClicked[post.ID][reaction.ReactionName] }" v-if="reaction.ReactionValue > 0">{{ reaction.ReactionValue }}</span>
+            
+            <EmojiLike class="emoji" v-if="reaction.ReactionName === 'Like'" @click="toggleReaction(post.ID, 'Like')" />
+            <EmojiLove class="emoji" v-if="reaction.ReactionName === 'Love'" @click="toggleReaction(post.ID, 'Love')" />
+            <EmojiHaha class="emoji" v-if="reaction.ReactionName === 'Haha'" @click="toggleReaction(post.ID, 'Haha')" />
+            <EmojiAngry class="emoji" v-if="reaction.ReactionName === 'Angry'" @click="toggleReaction(post.ID, 'Angry')" />
+            <EmojiSad class="emoji" v-if="reaction.ReactionName === 'Sad'" @click="toggleReaction(post.ID, 'Sad')" />
+            <EmojiWow class="emoji" v-if="reaction.ReactionName === 'Wow'" @click="toggleReaction(post.ID, 'Wow')" />
+            <EmojiYa class="emoji" v-if="reaction.ReactionName === 'Ya'" @click="toggleReaction(post.ID, 'Ya')" />
+          </div>
+
+          <div class="addEmoji">
+            <IconAddEmoji class="IconAddEmoji"/>
+
+            <div class="addEmoji-table">
+              <EmojiLike class="emoji" @click="toggleReaction(post.ID, 'Like')" />
+              <EmojiLove class="emoji" @click="toggleReaction(post.ID, 'Love')" />
+              <EmojiHaha class="emoji" @click="toggleReaction(post.ID, 'Haha')" />
+              <EmojiAngry class="emoji" @click="toggleReaction(post.ID, 'Angry')" />
+              <EmojiSad class="emoji" @click="toggleReaction(post.ID, 'Sad')" />
+              <EmojiWow class="emoji" @click="toggleReaction(post.ID, 'Wow')" />
+              <EmojiYa class="emoji" @click="toggleReaction(post.ID, 'Ya')" />
+            </div>
           </div>
         </div>
       </div>
@@ -161,19 +195,18 @@
   }
 
   .emoji {
-    width: 33px;
+    width: 29px;
     height: max-content;
   }
 
   .emoji:hover {
     cursor: pointer;
-    transform: scale(1.025);
     transition: 0.8s;
-    transform: rotate(360deg);
+    transform: scale(1.04) rotate(360deg);
   }
 
   .reactionValue {
-    height: 21px;
+    height: 20px;
     margin-right: -5px;
     padding: 0 9px 0 7px;
     display: flex;
@@ -181,7 +214,53 @@
     border-radius: 7px 0 0 7px;
     background-color: var(--color-contrast);
     color: var(--color-background1);
-    font-size: 20px;
+    font-size: 14px;
+  }
+
+  .reactionValue.active {
+    background-color: var(--color-lightBlue);
+  }
+
+  .addEmoji {
+    position: relative;
+    display: flex;
+  }
+
+  .addEmoji:hover > .addEmoji-table {
+    display: flex;
+  }
+
+  .IconAddEmoji {
+    position: relative;
+    width: 33px;
+    height: max-content;
+    fill: #FFDA6A;
+    margin-left: 22px;
+    z-index: 1;
+  }
+
+  .IconAddEmoji:hover {
+    cursor: pointer;
+    transition: 0.1s;
+    transform: scale(1.04);
+  }
+
+  .addEmoji-table {
+    position: absolute;
+    top: -2px;
+    right: -3px;
+    padding: 38px 6px 6px 6px;
+    display: none;
+    flex-direction: column;
+    background-color: var(--color-background2);
+    box-shadow: 0 0 16px 3px var(--color-background1);
+    border-radius: 12px;
+  }
+
+  .addEmoji-table > .emoji {
+    width: 27px;
+    margin-top: 7px;
+    margin-bottom: 7px;
   }
 
   /* main section */
