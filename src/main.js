7ac1cace8745a80e97c898a2b695e60cda3d6e99
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-03-31 20:32:27 +0200

    Add component registration

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/css/main.scss'
+import Avatar from './components/postsComponents/Avatar.vue'
 
 const app = createApp(App)
+app.component('Avatar', Avatar)
 
 app.use(router)
 
